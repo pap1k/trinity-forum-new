95e3451d9af16167ddc68326fe9667ad5667cc2f
pap1k <35275000+pap1k@users.noreply.github.com>
2022-08-18 17:10:23 +0300

    чето лень писать

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+from email import message
 from kernel.vk import VK
 from kernel.log import Log
 from kernel.parser import Parser, Forum, ForumException
 from os.path import exists
-import os, config, time
+import os, config, time, sys
 
 log = Log("[MAIN]").log
 if not exists(config.fileposted):
     open(config.fileposted, 'w').close()
 posted = [i for i in open(config.fileposted, 'r').read().split(',')]
+
+time_start = time.time()
 
 vk = VK(os.getenv("VK_TOKEN", default=""))
 parser = Parser(Forum(False), config.news_alerts, config.news_names, posted=posted)
@@ -29,6 +32,9 @@ while work:
             time.sleep(60*config.DELAY)
     except ForumException:
         log("Forum exception, re-init forum connection")
+        if "-test" in sys.argv:
+            vk.api("messages.send", peer_id=config.PROD_CONV_PEER, message="[TESTING]\nСоединение с форумом потеряно. С момента запуска (последнего обновления): "+str((time.time()-time_start)/60)+" минут")
+            time_start = time.time()
         parser.updateForum(Forum(False))
     except KeyboardInterrupt:
         work = False
